25df15f5cae8ccb99cea135f1464c5f406ea38d1
ade ilvi <adealvi@student.polindra.ac.id>
2023-11-29 10:09:04 +0700

    delete useless sidebar in role admin

--- src/components/layouts/SidebarComponents.vue
+++ src/components/layouts/SidebarComponents.vue
@@ -119,15 +119,7 @@
                 </CollapseItem>
             </template>
         </NavItem>
-        <NavItem v-if="$can('action', 'Admin')" span="Tes Risiko Stroke" icon="fa-stethoscope" target="#collapseRekap">
-            <template #item>
-                <CollapseItem header="Konsumen & Rekap" id="collapseRekap" title="Rekap Data">
-                    <template #router>
-                        <router-link class="collapse-item" to="/rekap_konsultasi">Tes Risiko Stroke</router-link>
-                    </template>
-                </CollapseItem>
-            </template>
-        </NavItem>
+    
         <NavItem span="Antrean Tes Stroke" v-if="$can('action', 'Dokter')" icon="fa-stethoscope" target="#collapseStroke">
             <template #item>
                 <CollapseItem header="" id="collapseStroke" title="Tes Risiko Stroke">
